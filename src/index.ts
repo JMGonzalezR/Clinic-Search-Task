fb7a60d13698c5203bb96c929d3da7403d7ca1a8
Juan M. Gonzalez R <juanml93@gmail.com>
2023-03-16 10:08:34 -0400

    adding the two requests and the filters for the endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,17 @@ import axios from "axios";
 
 interface Clinic {
     name: string;
-    state: string;
+    stateName: string;
     availability: {
+        from: string;
+        to: string;
+    };
+}
+
+interface VetClinic {
+    clinicName: string;
+    stateCode: string;
+    opening: {
         from: string;
         to: string;
     };
@@ -16,12 +25,50 @@ app.get("/clinics", async (req: Request, res: Response) => {
     const { name, state, from, to } = req.query;
 
     try {
-        const response = await axios.get<Clinic[]>(
+        const dentalClinicsResponse = await axios.get<Clinic[]>(
             "https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json"
         );
-        const clinics = response.data;
+        const dentalClinics = dentalClinicsResponse.data;
 
-        res.send(clinics);
+        const vetClinicsResponse = await axios.get<VetClinic[]>(
+            "https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json"
+        );
+        const vetClinics = vetClinicsResponse.data.map((clinic) => ({
+            name: clinic.clinicName,
+            stateName: clinic.stateCode,
+            availability: clinic.opening,
+        }));
+
+        console.log(dentalClinics);
+        const clinics: Clinic[] = [...dentalClinics, ...vetClinics];
+
+        const filteredClinics = clinics.filter((clinic) => {
+            if (
+                typeof name === "string" &&
+                clinic.name.toLowerCase().includes(name.toLowerCase())
+            ) {
+                return false;
+            }
+
+            if (
+                typeof state === "string" &&
+                clinic.stateName.toLowerCase().includes(state.toLowerCase())
+            ) {
+                return false;
+            }
+
+            if (typeof from === "string" && clinic.availability.from < from) {
+                return false;
+            }
+
+            if (typeof to === "string" && clinic.availability.to > to) {
+                return false;
+            }
+
+            return true;
+        });
+
+        res.status(200).json(filteredClinics);
     } catch (e) {
         console.error(e);
         res.status(500).send("Internal Server Error");
